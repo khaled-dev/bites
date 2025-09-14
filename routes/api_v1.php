03eb367a75ed1db5c5f91c93d8e157f102102a7d
Khaled AbdElRahim <khaled710017@gmail.com>
2025-09-14 21:31:52 +0300

    Handle storage permissions

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\FileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::get('/users', [UserController::class, 'index'])->name("users.index");
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+    Route::post('/files', [FileController::class, 'store'])->name('files.store');
+    Route::get('/files/{file}', [FileController::class, 'show'])->name('files.store');
 });
 
 
