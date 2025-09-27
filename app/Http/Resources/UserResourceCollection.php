b8b5185a5f561d9ff0e52bb8da84222a491bbb8d
Khaled AbdElRahim <khaled710017@gmail.com>
2025-09-27 16:18:08 +0300

    Create user resource collection

---
+++ app/Http/Resources/UserResourceCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class UserResourceCollection extends ResourceCollection
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'status' => 200,
+            'message' => 'Users retrieved successfully',
+            'data' => $this->collection,
+        ];
+    }
+}
